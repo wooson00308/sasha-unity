7c665af68ae34c6e287aa2784af77ddaa32f3c5f
Thon <wooson0308@gmail.com>
2025-05-21 18:13:57 +0900

    Refactor: 상태 효과 핸들러 로그 통일성 개선 (파일럿 이름 -> 기체 이름)

--- Assets/AF/Scripts/Combat/Handlers/APRecoveryBoostHandler.cs
+++ Assets/AF/Scripts/Combat/Handlers/APRecoveryBoostHandler.cs
@@ -9,14 +9,14 @@ namespace AF.Combat.Handlers
         public void OnApply(CombatContext ctx, ArmoredFrame target, StatusEffect effect)
         {
             target.CombinedStats.ApplyModifier(StatType.APRecovery, effect.ModificationType, effect.ModificationValue);
-            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Pilot.Name}</color> 기체 에너지 회복 시스템 <color=green>최적화</color>. (AP 회복량 증가: {effect.ModificationValue}, 지속시간: {effect.DurationTurns}턴)", LogLevel.Info, LogEventType.StatusEffectApplied);
+            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Name}</color> 기체 에너지 회복 시스템 <color=green>최적화</color>. (AP 회복량 증가: {effect.ModificationValue}, 지속시간: {effect.DurationTurns}턴)", LogLevel.Info, LogEventType.StatusEffectApplied);
         }
 
         public void OnTick(CombatContext ctx, ArmoredFrame target, StatusEffect effect) { }
 
         public void OnExpire(CombatContext ctx, ArmoredFrame target, StatusEffect effect)
         {
-            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Pilot.Name}</color> 기체 에너지 회복 최적화 효과 <color=yellow>만료</color>.", LogLevel.Info, LogEventType.StatusEffectExpired);
+            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Name}</color> 기체 에너지 회복 최적화 효과 <color=yellow>만료</color>.", LogLevel.Info, LogEventType.StatusEffectExpired);
         }
 
         public void OnRemove(CombatContext ctx, ArmoredFrame target, StatusEffect effect)
@@ -24,7 +24,7 @@ namespace AF.Combat.Handlers
             var reverseModificationType = effect.ModificationType == ModificationType.Additive ? ModificationType.Additive : ModificationType.Multiplicative;
             var reverseValue = effect.ModificationType == ModificationType.Additive ? -effect.ModificationValue : (1f / effect.ModificationValue);
             target.CombinedStats.ApplyModifier(StatType.APRecovery, reverseModificationType, reverseValue);
-            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Pilot.Name}</color> 기체 에너지 회복 시스템 <color=red>원상 복구</color>.", LogLevel.Info, LogEventType.StatusEffectRemoved);
+            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Name}</color> 기체 에너지 회복 시스템 <color=red>원상 복구</color>.", LogLevel.Info, LogEventType.StatusEffectRemoved);
         }
     }
 } 
--- Assets/AF/Scripts/Combat/Handlers/DefenseReducedHandler.cs
+++ Assets/AF/Scripts/Combat/Handlers/DefenseReducedHandler.cs
@@ -9,19 +9,19 @@ namespace AF.Combat.Handlers
         public void OnApply(CombatContext ctx, ArmoredFrame target, StatusEffect effect)
         {
             target.CombinedStats.ApplyModifier(StatType.Defense, effect.ModificationType, effect.ModificationValue);
-            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Pilot.Name}</color> 기체 장갑 <color=red>약화</color> 발생. (효과: {effect.ModificationValue:P0}, 지속시간: {effect.DurationTurns}턴)", LogLevel.Info, LogEventType.StatusEffectApplied);
+            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Name}</color> 기체 장갑 <color=red>약화</color> 발생. (효과: {effect.ModificationValue:P0}, 지속시간: {effect.DurationTurns}턴)", LogLevel.Info, LogEventType.StatusEffectApplied);
         }
         public void OnTick(CombatContext ctx, ArmoredFrame target, StatusEffect effect) { }
         public void OnExpire(CombatContext ctx, ArmoredFrame target, StatusEffect effect)
         {
-            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Pilot.Name}</color> 기체 장갑 약화 효과 <color=yellow>소멸</color>.", LogLevel.Info, LogEventType.StatusEffectExpired);
+            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Name}</color> 기체 장갑 약화 효과 <color=yellow>소멸</color>.", LogLevel.Info, LogEventType.StatusEffectExpired);
         }
         public void OnRemove(CombatContext ctx, ArmoredFrame target, StatusEffect effect)
         {
             var reverseModificationType = effect.ModificationType == ModificationType.Additive ? ModificationType.Additive : ModificationType.Multiplicative;
             var reverseValue = effect.ModificationType == ModificationType.Additive ? -effect.ModificationValue : (1f / effect.ModificationValue);
             target.CombinedStats.ApplyModifier(StatType.Defense, reverseModificationType, reverseValue);
-            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Pilot.Name}</color> 기체 장갑 약화 효과 <color=green>복구</color>.", LogLevel.Info, LogEventType.StatusEffectRemoved);
+            ctx.Logger.TextLogger.Log($"<color=#00FFFF>{target.Name}</color> 기체 장갑 약화 효과 <color=green>복구</color>.", LogLevel.Info, LogEventType.StatusEffectRemoved);
         }
     }
 } 
